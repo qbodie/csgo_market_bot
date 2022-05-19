94b16b7a39b60eb458f07997c748834b5a5e99d6
qbodie <qbodie0@gmail.com>
2022-05-19 23:46:37 +0300

    fill JSON-file by informations

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 import requests
 
 url = 'https://market.csgo.com'
-result = {}
+result = []
 
 def collect_data():
     response = requests.get(url='https://market.csgo.com/?s=pop&t=365&sd=desc')
@@ -18,9 +18,20 @@ def collect_data():
         item_url = url + a_item_url.get('href')
         item_price = a_item_url.find('div', class_='price').text.strip()
         item_name = a_item_url.find('div', class_='name').text.strip()
-        print(item_name)
-        print(item_url)
-        print(item_price)
+        # print(item_name)
+        # print(item_url)
+        # print(item_price)
+        result.append(
+            {
+                "item_name": item_name,
+                "item_price": item_price,
+                "item_url": item_url
+            }
+        )
+    # print(len(result))
+    with open('result.json', 'w', encoding='utf-8') as file:
+        json.dump(result, file, indent=4, ensure_ascii=False)
+
 
 
 
